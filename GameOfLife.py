784fcfaa672e42b9539b17458355c046c0cc08de
AubinArrouet <aubin.arrouet@gmail.com>
2025-01-13 18:17:09 +0100

    first version

---
+++ GameOfLife.py
@@ -0,0 +1,64 @@
+def initialize():
+    current_state = [
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                 #            ',
+        '                                ##            ',
+        '                                 ##           ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              ',
+        '                                              '
+    ]
+
+    for generation in range(1000):  # Stops at 1,000 generations
+        print(f"Generation {generation}:")
+        for row in current_state:
+            print(row)
+        print("\n")
+        current_state = iteration(current_state)
+
+def iteration(current_state):
+    rows = len(current_state)
+    cols = len(current_state[0])
+    new_state = []
+
+    for i in range(rows):
+        new_row = ''
+        for j in range(cols):
+            alive_neighbors = count_alive_neighbors(current_state, i, j,rows,cols)
+            if current_state[i][j] == '#' and alive_neighbors in [2, 3]:
+                new_row += '#'
+            elif current_state[i][j] == ' ' and alive_neighbors == 3:
+                new_row += '#'
+            else:
+                new_row += ' '
+        new_state.append(new_row)
+
+    return new_state
+
+def count_alive_neighbors(current_state, x, y,rows,cols):
+    alive_neighbors = 0
+
+    for dx in [-1, 0, 1]:
+        for dy in [-1, 0, 1]:
+            if dx == 0 and dy == 0:
+                continue
+
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < rows and 0 <= ny < cols and current_state[nx][ny] == '#':
+                alive_neighbors += 1
+
+    return alive_neighbors
+
+if __name__ == "__main__":
+    initialize()
